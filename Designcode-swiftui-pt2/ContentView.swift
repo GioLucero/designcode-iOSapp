f9d93e435f9f846b7c7f2b75a9c1c78b0ea0b485
GioLucero <gioandrewlucero@gmail.com>
2020-02-03 11:07:18 -0500

    Initial Commit

---
+++ Designcode-swiftui-pt2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Designcode-swiftui-pt2
+//
+//  Created by Gio Lucero on 2020-02-03.
+//  Copyright © 2020 Gio Lucero. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
